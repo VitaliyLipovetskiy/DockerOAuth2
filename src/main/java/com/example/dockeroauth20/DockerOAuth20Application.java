556e38947a36d57da78ee9a90e4510718df09787
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2022-11-30 14:24:33 +0200

    add docker

---
+++ src/main/java/com/example/dockeroauth20/DockerOAuth20Application.java
@@ -0,0 +1,13 @@
+package com.example.dockeroauth20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerOAuth20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerOAuth20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockeroauth20/DockerOAuth20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockeroauth20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerOAuth20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
